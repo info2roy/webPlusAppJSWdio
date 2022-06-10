7bd60df390a5f63a870a3400031d9d41f0a384a6
Mahesh Bale <mahesh.bale@scripbox.com>
2022-06-10 12:26:17 +0530

    updated selector and removed contains

--- src/main/PageObjects/PersonalInfoPageObjects.js
+++ src/main/PageObjects/PersonalInfoPageObjects.js
@@ -1,9 +1,9 @@
 class PersonalInfoPageObjects {
 
-    updatePicture = "//div[contains(text(), 'Update Picture')]"; //update picture link
+    updatePicture = "//div[text()='Update Picture']"; //update picture link
     fileUploadButton = "p.hide-on-med-and-down"; //File upload button
     inputFile = "input#file";  //File input dialog
-    submitButton = "//button[contains(text(), 'Submit')]";
+    submitButton = "//button[text()='Submit']";
 }
 
 module.exports = new PersonalInfoPageObjects()
