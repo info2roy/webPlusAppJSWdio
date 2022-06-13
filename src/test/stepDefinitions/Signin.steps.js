46b1526914f5e9c782bd01440154829d24add0f4
Mahesh Bale <mahesh.bale@scripbox.com>
2022-06-13 18:58:10 +0530

    added expects for signin pages

--- src/test/stepDefinitions/Signin.steps.js
+++ src/test/stepDefinitions/Signin.steps.js
@@ -1,17 +1,17 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
 const signinFunc = require('../../main/PageFunctionalities/SigninFunctionality');
-
+const expect = require('chai').expect;
 
 When(/^I login with username$/, async () => {
     console.log("When I login with username");
+    expect(await signinFunc.firstLoginPageLaunched()).to.be.true;
     await signinFunc.loginWithUsername("test+180756@scripbox.io");
-    await browser.pause(1000);
 });
 
 When(/^I login to scripbox with username and password$/, async () => {
     console.log("When I login with username and password");
+    expect(await signinFunc.secondLoginPageLaunched()).to.be.true;
     await signinFunc.loginWithUsernameAndPassword("test+180756@scripbox.io", "folio334!cues");
     console.log("filled username and password");
-    await browser.pause(1000);
 });
 
