1dca087f05ad362e6db92da5e52f2724d03f293f
Mahesh Bale <mahesh.bale@scripbox.com>
2022-06-21 17:28:49 +0530

    removed unused object from HomePage and LoginPage

--- src/main/PageObjects/HomePageObjects.js
+++ src/main/PageObjects/HomePageObjects.js
@@ -1,16 +1,4 @@
 class HomePageObjects {
-    loginTab = "a[data-action='menu login']"; //open login page from home page via this tab
-    signupTab = "a[data-action='menu signup']"; //open signup page from home page via this tab
-
-    //Responsive UI related objects
-    //menuButton = "//button[@data-action='menu hamburger']" //three line menu button on response web UI
-
-    // Android locators
-
-    loginButtonHomePage_android = "//*[@text='Login']"
-    ScripboxCustomerBanner = "//*[@text='Login']"
-    moreOptions_android = '//*[contains(@content-desc,"More")]'
-
     //refactored objects
     menuButton = {web: "//button[@data-action='menu hamburger']"};
     loginButton = {web: "a[data-action='menu login']", androidapp: "//*[@text='Login']"};
--- src/main/PageObjects/LoginPageObjects.js
+++ src/main/PageObjects/LoginPageObjects.js
@@ -1,24 +1,5 @@
 class LoginPageObjects {
-
-    scripboxLoginImage = '.desktop-logo';
-    loginButton = '//button[text()="Login"]';
-    //userId = '#test-email-input'; //phone no or email
-    email = 'input#user_email' //email for the login form
-    password = 'input#user_password'; //password for the login form
-    //continueLoginButton = '#test-login-next-button'; //Continue login after filling the userId(phone no or email)
-    continueSigninButton = 'button#test-signup-next-button'; //Continue signin after filling the username and password
-
-
-    // Android Objects
-    
-    loginSignupButton_android = "//*[@text='Login or Signup']"
-    nextButtonLoginPage_android = "//*[@text='NEXT']"
-    continueButton_android = "//*[@text='CONTINUE']"
-    loginSignUpHeader_android = "//*[@text='Login / Signup']"
-
-
-    loginCredentialsText_android = '//*[@class="android.widget.EditText" and @package="com.scripbox.takecharge"]'
-    
+    loginCredentialsText_android = '//*[@class="android.widget.EditText" and @package="com.scripbox.takecharge"]'    
 
     //common object
     userIdField = {web: '#test-email-input', androidapp: this.loginCredentialsText_android};
