f386a286cbd1f1ff3cfccda031bf23a03806c5c1
Mahesh Bale <mahesh.bale@scripbox.com>
2022-06-22 17:06:16 +0530

    fixed web selector for helloUser

--- src/main/PageObjects/DashboardPageObjects.js
+++ src/main/PageObjects/DashboardPageObjects.js
@@ -1,6 +1,6 @@
 class DashboardPageObjects {
 
-    helloUser = {web: "//*[@text='HELLO']", androidapp: "//*[@text='HELLO']"};
+    helloUser = {web: "//div[text()='HELLO']", androidapp: "//*[@text='HELLO']"};
     moreButton = {web: "//div[text()='More']", desktop: "span.desktop-dropdown-icon", 
         androidapp: "//android.widget.TextView[@text='More']"}
     personalInfoOption = {web: "//div[text()='Personal Information']", desktop: "span.test-personal-information",
