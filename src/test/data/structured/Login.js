10a8956cc008fcb368ed695129a667feb9cdf612
Mahesh Bale <mahesh.bale@scripbox.com>
2022-06-24 17:20:05 +0530

    removed hardcoding of login credentials

---
+++ src/test/data/structured/Login.js
@@ -0,0 +1,4 @@
+module.exports = {
+  username: 'test+180756@scripbox.io',
+  password: 'folio334!cues'
+};
--- src/test/stepDefinitions/LoginSteps.js
+++ src/test/stepDefinitions/LoginSteps.js
@@ -1,15 +1,27 @@
 const { When } = require('@wdio/cucumber-framework');
 const { expect } = require('chai');
 const loginFunc = require('../../main/PageFunctionalities/LoginFunctionality');
+const loginCreds = require('../data/structured/Login');
 
 When(/^I login with username$/, async () => {
   await console.log('When I login with username');
-  await loginFunc.loginWithUsername('test+180756@scripbox.io'); // uat
+  await loginFunc.loginWithUsername(loginCreds.username);
   expect(await loginFunc.secondLoginPageLaunched()).to.be.true;
 });
 
 When(/^I login with username and password$/, async () => {
   await console.log('When I login with username and password');
-  await loginFunc.loginWithUsernameAndPassword('test+180756@scripbox.io', 'folio334!cues');
+  await loginFunc.loginWithUsernameAndPassword(loginCreds.username, loginCreds.password);
   await console.log('Filled username and password');
+});
+
+When(/^I login with "([^"]*)"$/, async (username) => {
+  await console.log(`When I login with username:${username}`);
+  await loginFunc.loginWithUsername(username);
+  expect(await loginFunc.secondLoginPageLaunched()).to.be.true;
+});
+
+When(/^I login with "([^"]*)" and "([^"]*)"$/, async (username, password) => {
+  await console.log(`When I login with username:${username} and password:${password}`);
+  await loginFunc.loginWithUsernameAndPassword(username, password);
 });
