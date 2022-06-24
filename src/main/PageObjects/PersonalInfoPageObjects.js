6dd5af33d7d4ba4a8c949b9672fffb9e6f54b915
Mahesh Bale <mahesh.bale@scripbox.com>
2022-06-24 15:47:13 +0530

    use double quotes instead of \'

--- src/main/PageObjects/PersonalInfoPageObjects.js
+++ src/main/PageObjects/PersonalInfoPageObjects.js
@@ -1,30 +1,30 @@
 class PersonalInfoPageObjects {
   fileInput = { web: 'input#file' }; // file input tag for uploading profile picture on web => <input type="file">
 
-  submitPictureButton = { web: '//button[text()=\'Submit\']' }; // The submit button for updated profile picture on web
+  submitPictureButton = { web: '//button[text()="Submit"]' }; // The submit button for updated profile picture on web
 
-  profileInfomationHeader = { web: '(//div[text()=\'Personal Information\'])[4]',
-    app: '//*[@text=\'Profile information\']' }; // profile information page
+  profileInfomationHeader = { web: '(//div[text()="Personal Information"])[4]',
+    app: '//*[@text="Profile information"]' }; // profile information page
 
-  updatePictureOption = { web: '//div[text()=\'Update Picture\']',
-    app: '//*[@text=\'Update picture\']' }; // update picture button
+  updatePictureOption = { web: '//div[text()="Update Picture"]',
+    app: '//*[@text="Update picture"]' }; // update picture button
 
   // The dialog for "Allow Scripbox to take pictures and record video?"
-  alertForAllowCameraAccess = { app: '//*[@text=\'Allow Scripbox to take pictures and record video?\']' };
+  alertForAllowCameraAccess = { app: '//*[@text="Allow Scripbox to take pictures and record video?"]' };
 
   // The option "While using the app"
-  alertForAllowCameraAccess_whileUsingTheAppOption = { app: '//*[@text=\'While using the app\']' };
+  alertForAllowCameraAccess_whileUsingTheAppOption = { app: '//*[@text="While using the app"]' };
 
   // The dialog for "Allow Scripbox to access photos and media on your device?"
-  alertForAllowMediaAccess = { app: '//*[@text=\'Allow Scripbox to access photos and media on your device?\']' };
+  alertForAllowMediaAccess = { app: '//*[@text="Allow Scripbox to access photos and media on your device?"]' };
 
-  alertForAllowMediaAccess_allowOption = { app: '//*[@text=\'Allow\']' }; // The option "Allow"
+  alertForAllowMediaAccess_allowOption = { app: '//*[@text="Allow"]' }; // The option "Allow"
 
   clickPictureButton = { app: '~Shutter' }; // click picture shutter button
 
   donePictureButton = { app: '~Done' }; // the "Done" button for click picture
 
-  editPhotoHeader = { app: '//*[@text=\'Edit Photo\']' }; // The "Edit Photo Page"
+  editPhotoHeader = { app: '//*[@text="Edit Photo"]' }; // The "Edit Photo Page"
 
   doneCropButton = { app: '~Crop' }; // the done Crop button
 }
