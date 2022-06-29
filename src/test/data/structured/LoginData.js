f1ada57999535a99b64fbc5bed543d5163cf4c74
Mahesh Bale <mahesh.bale@scripbox.com>
2022-06-29 09:54:44 +0530

    use uat creds for LoginData.js

--- src/test/data/structured/LoginData.js
+++ src/test/data/structured/LoginData.js
@@ -1,6 +1,4 @@
 module.exports = {
-  //username: 'test+180756@scripbox.io',
-  //password: 'folio334!cues'
-  username: 'mahesh.bale@scripbox.com',
-  password: 'Aaasssddd123$'
+  username: 'test+180756@scripbox.io',
+  password: 'folio334!cues'
 };
