d79f4aed6cdbf3768860595ffb4de8f1ab1e4304
Mahesh Bale <mahesh.bale@scripbox.com>
2022-07-12 13:10:07 +0530

    fixed issue with responsive flow to go back to dashbaord

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
 module.exports = {
-  myScripBoxUAT2: 'https://uat-myscripbox-2-uat.scripbox.org/',
-  andromedaUAT2: 'https://uat-andromeda-2-uat.scripbox.org/'
+  myScripBox: 'https://uat-myscripbox-2-uat.scripbox.org/',
+  andromeda: 'https://uat-andromeda-2-uat.scripbox.org/'
 };
--- src/main/Functionalities/DashboardFunctionality.js
+++ src/main/Functionalities/DashboardFunctionality.js
@@ -2,12 +2,13 @@ const DashboardPage = require('../Pages/DashboardPage');
 const DashboardObjects = require('../Objects/DashboardObjects');
 const Utils = require('../../support/Utils/Utils');
 const Device = require('../../support/libraries/Device');
+const env = require('../../config/env');
 
 class DashboardFunctionality {
 
   async open() {
     if (Device.isWeb()) {
-      await browser.url('/dashboard');
+      await browser.url(`${env.myScripBox}dashboard`);
     } else if (Device.isAndroidApp()) {
       await browser.url('scripbox://scripbox');
     }
