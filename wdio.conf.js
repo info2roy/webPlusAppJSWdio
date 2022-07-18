454356a53403dfef93866ea5e9a97414cd33b419
Mahesh Bale <mahesh.bale@scripbox.com>
2022-07-18 17:22:44 +0530

    added hooks commented code for cucumber hooks

--- wdio.conf.js
+++ wdio.conf.js
@@ -330,4 +330,64 @@ exports.config = {
     */
   // onReload: function(oldSessionId, newSessionId) {
   // }
+
+  /**
+    * Cucumber Hooks
+    *
+    * Runs before a Cucumber Feature.
+    * @param {String}                   uri      path to feature file
+    * @param {GherkinDocument.IFeature} feature  Cucumber feature object
+    */
+  // beforeFeature: function (uri, feature) {
+  // },
+  /**
+    *
+    * Runs before a Cucumber Scenario.
+    * @param {ITestCaseHookParameter} world    world object containing information on pickle and test step
+    * @param {Object}                 context  Cucumber World object
+    */
+  // beforeScenario: function (world, context) {
+  // },
+  /**
+    *
+    * Runs before a Cucumber Step.
+    * @param {Pickle.IPickleStep} step     step data
+    * @param {IPickle}            scenario scenario pickle
+    * @param {Object}             context  Cucumber World object
+    */
+  // beforeStep: function (step, scenario, context) {
+  // },
+  /**
+    *
+    * Runs after a Cucumber Step.
+    * @param {Pickle.IPickleStep} step             step data
+    * @param {IPickle}            scenario         scenario pickle
+    * @param {Object}             result           results object containing scenario results
+    * @param {boolean}            result.passed    true if scenario has passed
+    * @param {string}             result.error     error stack if scenario failed
+    * @param {number}             result.duration  duration of scenario in milliseconds
+    * @param {Object}             context          Cucumber World object
+    */
+  // afterStep: function (step, scenario, result, context) {
+  // },
+  /**
+    *
+    * Runs after a Cucumber Scenario.
+    * @param {ITestCaseHookParameter} world            world object containing information on pickle and test step
+    * @param {Object}                 result           results object containing scenario results `{passed: boolean, error: string, duration: number}`
+    * @param {boolean}                result.passed    true if scenario has passed
+    * @param {string}                 result.error     error stack if scenario failed
+    * @param {number}                 result.duration  duration of scenario in milliseconds
+    * @param {Object}                 context          Cucumber World object
+    */
+  // afterScenario: function (world, result, context) {
+  // },
+  /**
+    *
+    * Runs after a Cucumber Feature.
+    * @param {String}                   uri      path to feature file
+    * @param {GherkinDocument.IFeature} feature  Cucumber feature object
+    */
+  // afterFeature: function (uri, feature) {
+  // }
 };
