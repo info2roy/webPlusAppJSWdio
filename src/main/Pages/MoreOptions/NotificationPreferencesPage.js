79aa17fbc12d6ca494e01cbcfb9f269d1bd21c0a
Mahesh Bale <mahesh.bale@scripbox.com>
2022-07-20 17:26:20 +0530

    rename of file

--- src/main/Pages/MoreOptions/NotificationPreferencesPage.js
+++ src/main/Pages/MoreOptions/NotificationPreferencesPage.js
@@ -1,17 +1,17 @@
 const Utils = require('../../../support/Utils/Utils');
-const NotificationPreferenceObjects = require('../../Objects/MoreOptions/NotificationPreferenceObjects');
+const NotificationPreferencesObjects = require('../../Objects/MoreOptions/NotificationPreferencesObjects');
 const { expect } = require('chai');
 
 class NotificationPreferencesPage {
   async notificationPreferencesPageHeaderIsDisplayed() {
-    return await Utils.elementIsDisplayed(NotificationPreferenceObjects.notificationPreferencesPageHeader);
+    return await Utils.elementIsDisplayed(NotificationPreferencesObjects.notificationPreferencesPageHeader);
   }
 
   async notificationPreferenceControlsAreDisplayedAndAreCorrect() {
     let allDisplayed = true;
-    for (const [key, value] of Object.entries(NotificationPreferenceObjects.notificationPreferenceControls)) {
+    for (const [key, value] of Object.entries(NotificationPreferencesObjects.notificationPreferenceControls)) {
       allDisplayed = allDisplayed && (await Utils.elementIsDisplayed(
-        NotificationPreferenceObjects.notificationPreferenceType(key))
+        NotificationPreferencesObjects.notificationPreferenceType(key))
       );
       allDisplayed = allDisplayed && (await Utils.elementIsDisplayed(value.email)) &&
         (await Utils.elementIsDisplayed(value.sms));
@@ -29,40 +29,40 @@ class NotificationPreferencesPage {
   }
 
   async notificationPreferencesControlsTestToggleScenario(controlName, channel) {
-    const selector = NotificationPreferenceObjects.notificationPreferenceControls[controlName][channel];
+    const selector = NotificationPreferencesObjects.notificationPreferenceControls[controlName][channel];
     console.log(`${controlName}: ${JSON.stringify(selector)}`);
     const initialSrc = await Utils.getElementAttributeBySelector(selector, 'src');
     await Utils.clickElement(selector);
     const firstToggleSrc = await Utils.getElementAttributeBySelector(selector, 'src');
-    expect(firstToggleSrc).to.be.equal(NotificationPreferenceObjects.toggleScenarios[initialSrc]);
-    await Utils.clickElement(NotificationPreferenceObjects.updateChangesButton);
-    expect(await Utils.elementIsDisplayed(NotificationPreferenceObjects.updateSuccessMessage)).to.be.true;
+    expect(firstToggleSrc).to.be.equal(NotificationPreferencesObjects.toggleScenarios[initialSrc]);
+    await Utils.clickElement(NotificationPreferencesObjects.updateChangesButton);
+    expect(await Utils.elementIsDisplayed(NotificationPreferencesObjects.updateSuccessMessage)).to.be.true;
     await Utils.clickElement(selector);
     const secondToggleSrc = await Utils.getElementAttributeBySelector(selector, 'src');
-    expect(secondToggleSrc).to.be.equal(NotificationPreferenceObjects.toggleScenarios[firstToggleSrc]);
-    await Utils.clickElement(NotificationPreferenceObjects.updateChangesButton);
-    expect(await Utils.elementIsDisplayed(NotificationPreferenceObjects.updateSuccessMessage)).to.be.true;
+    expect(secondToggleSrc).to.be.equal(NotificationPreferencesObjects.toggleScenarios[firstToggleSrc]);
+    await Utils.clickElement(NotificationPreferencesObjects.updateChangesButton);
+    expect(await Utils.elementIsDisplayed(NotificationPreferencesObjects.updateSuccessMessage)).to.be.true;
   }
 
   async notificationPreferenceControlsRandomToggleScenario() {
-    Object.entries(NotificationPreferenceObjects.notificationPreferenceControls).forEach(async ([, value]) => {
+    Object.entries(NotificationPreferencesObjects.notificationPreferenceControls).forEach(async ([, value]) => {
       let num = Utils.getRandomInt(2);
       if (num === 1) {
         const initialSrc = await Utils.getElementAttributeBySelector(value.email, 'src');
         await Utils.clickElement(value.email);
         const toggleSrc = await Utils.getElementAttributeBySelector(value.email, 'src');
-        expect(toggleSrc).to.be.equal(NotificationPreferenceObjects.toggleScenarios[initialSrc]);
+        expect(toggleSrc).to.be.equal(NotificationPreferencesObjects.toggleScenarios[initialSrc]);
       }
       num = Utils.getRandomInt(2);
       if (num === 1) {
         const initialSrc = await Utils.getElementAttributeBySelector(value.sms, 'src');
         await Utils.clickElement(value.sms);
         const toggleSrc = await Utils.getElementAttributeBySelector(value.sms, 'src');
-        expect(toggleSrc).to.be.equal(NotificationPreferenceObjects.toggleScenarios[initialSrc]);
+        expect(toggleSrc).to.be.equal(NotificationPreferencesObjects.toggleScenarios[initialSrc]);
       }
     });
-    await Utils.clickElement(NotificationPreferenceObjects.updateChangesButton);
-    expect(await Utils.elementIsDisplayed(NotificationPreferenceObjects.updateSuccessMessage)).to.be.true;
+    await Utils.clickElement(NotificationPreferencesObjects.updateChangesButton);
+    expect(await Utils.elementIsDisplayed(NotificationPreferencesObjects.updateSuccessMessage)).to.be.true;
   }
 }
 module.exports = new NotificationPreferencesPage();
