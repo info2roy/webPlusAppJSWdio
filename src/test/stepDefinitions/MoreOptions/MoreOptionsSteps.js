70d0872dd7fc9ab36e772362b2ce26195fb42c38
Mahesh Bale <mahesh.bale@scripbox.com>
2022-07-26 13:09:56 +0530

    removed small steps files

--- src/test/stepDefinitions/MoreOptions/MoreOptionsSteps.js
+++ src/test/stepDefinitions/MoreOptions/MoreOptionsSteps.js
@@ -2,6 +2,8 @@ const { When, Then } = require('@wdio/cucumber-framework');
 const { expect } = require('chai');
 const MoreOptionsFunctionality = require('../../../main/Functionalities/MoreOptions/MoreOptionsFunctionality');
 const DashboardFunctionality = require('../../../main/Functionalities/DashboardFunctionality');
+const NotificationPreferencesFunctionality = require('../../../main/Functionalities/MoreOptions/NotificationPreferencesFunctionality');
+const WhatappNotificationsFunctionality = require('../../../main/Functionalities/MoreOptions/WhatappNotificationsFunctionality');
 
 Then(/^I should see all of the more options$/, async () => {
   await console.log('Then I should see all of the more options');
@@ -28,4 +30,19 @@ Then(/^I logout of Scripbox$/, async () => {
   await DashboardFunctionality.open();
   await DashboardFunctionality.openMoreOptions();
   await MoreOptionsFunctionality.selectLogout();
+});
+
+Then(/^I can update notifications preference control (.+) for channel (.+)$/, async (controlName, channel) => {
+  await console.log(`I can update control ${controlName} for channel ${channel}`);
+  NotificationPreferencesFunctionality.testToggleScenario(controlName, channel);
+});
+
+Then(/^I can update random group of notifications preference controls$/, async () => {
+  await console.log('Then I can update random group of notifications preference controls');
+  NotificationPreferencesFunctionality.testRandomToggleScenarios();
+});
+
+Then(/^I can update whatsapp notifications control$/, async () => {
+  await console.log('Then I can update whatsapp notifications control');
+  await WhatappNotificationsFunctionality.testToggleScenario();
 });
--- src/test/stepDefinitions/MoreOptions/WhatsappNotificationsSteps.js
+++
@@ -1,7 +0,0 @@
-const { Then } = require('@wdio/cucumber-framework');
-const WhatappNotificationsFunctionality = require('../../../main/Functionalities/MoreOptions/WhatappNotificationsFunctionality');
-
-Then(/^I can update whatsapp notifications control$/, async () => {
-  await console.log('Then I can update whatsapp notifications control');
-  await WhatappNotificationsFunctionality.testToggleScenario();
-});
