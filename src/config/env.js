94d403b7c38eeca6c51cc4c16eabcadc352a9b0a
Parthesh Parekh <parthesh.parekh@scripbox.com>
2022-08-03 17:26:40 +0530

    update confi for system vars

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
 module.exports = {
-  myScripBox: 'https://uat-myscripbox-2-uat.scripbox.org/',
-  andromeda: 'https://uat-andromeda-2-uat.scripbox.org/',
-  andromedaUat38: 'https://uat-andromeda-38-uat.scripbox.org',
-  mockStaging: 'https://stag-myscripbox-2-feature-qa-automation.scripbox.org/',
+  MYSCRIPBOX: process.env.MYSCRIPBOX_URL || '',
+  MYSCRIPBOX_API: process.env.MYSCRIPBOX_API_URL || '',
+  ANDROMEDA: process.env.ANDROMEDA_URL || '',
+  MOCKAPI: process.env.MOCKAPI_URL || '',
   sleepTimeInMsAfterEachStep: 0
 };
