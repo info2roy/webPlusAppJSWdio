a60ee095a9bc3c398c3815dda9f56a5a9099c987
Mahesh Bale <mahesh.bale@scripbox.com>
2022-08-18 18:48:03 +0530

    changes in .env file

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
 module.exports = {
-  MYSCRIPBOX: process.env.MYSCRIPBOX_URL || '',
-  MYSCRIPBOX_API: process.env.MYSCRIPBOX_API_URL || '',
-  ANDROMEDA: process.env.ANDROMEDA_URL || '',
-  MOCKAPI: process.env.MOCKAPI_URL || '',
-  SLEEP_TIME_IN_MS_AFTER_EACH_STEP: process.env.SLEEP_MS_AFTER_EACH_STEP || 0
+  MYSCRIPBOX: process.env.MYSCRIPBOX_URL_CI || process.env.MYSCRIPBOX_URL,
+  MYSCRIPBOX_API: process.env.MYSCRIPBOX_API_URL_CI || process.env.MYSCRIPBOX_API_URL,
+  ANDROMEDA: process.env.ANDROMEDA_URL_CI || process.env.ANDROMEDA_URL,
+  MOCKAPI: process.env.MOCKAPI_URL_CI || process.env.MOCKAPI_URL,
+  SLEEP_TIME_IN_MS_AFTER_EACH_STEP: process.env.SLEEP_MS_AFTER_EACH_STEP_CI || process.env.SLEEP_MS_AFTER_EACH_STEP || 0
 };
