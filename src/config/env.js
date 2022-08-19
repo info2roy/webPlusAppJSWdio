e21fd39dbd57a16f5c40880df0a1b4f65e9de819
Mahesh Bale <mahesh.bale@scripbox.com>
2022-08-19 11:18:01 +0530

    updated variables

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,8 @@
 module.exports = {
-  MYSCRIPBOX: process.env.MYSCRIPBOX_URL_CI || process.env.MYSCRIPBOX_URL,
-  MYSCRIPBOX_API: process.env.MYSCRIPBOX_API_URL_CI || process.env.MYSCRIPBOX_API_URL,
-  ANDROMEDA: process.env.ANDROMEDA_URL_CI || process.env.ANDROMEDA_URL,
-  MOCKAPI: process.env.MOCKAPI_URL_CI || process.env.MOCKAPI_URL,
-  SLEEP_TIME_IN_MS_AFTER_EACH_STEP: process.env.SLEEP_MS_AFTER_EACH_STEP_CI || process.env.SLEEP_MS_AFTER_EACH_STEP || 0
+  MYSCRIPBOX: process.env.MYSCRIPBOX_URL || process.env.MYSCRIPBOX_URL_CI || 'https://stag-myscripbox-2-feature-qa-automation.scripbox.org',
+  MYSCRIPBOX_API: process.env.MYSCRIPBOX_API_URL || process.env.MYSCRIPBOX_API_URL_CI || 'https://stag-myscripboxapi-2-feature-qa-automation.scripbox.org',
+  ANDROMEDA: process.env.ANDROMEDA_URL || process.env.ANDROMEDA_URL_CI || 'https://stag-andromeda-2-feature-qa-automation.scripbox.org',
+  MOCKAPI: process.env.MOCKAPI_URL || process.env.MOCKAPI_URL_CI || 'https://inte-mockapi-128-master.scripbox.org',
+  SELENIUM_HUB: process.env.SELENIUM_HUB_URL || process.env.SELENIUM_HUB_URL_CI,
+  SLEEP_TIME_IN_MS_AFTER_EACH_STEP: process.env.SLEEP_MS_AFTER_EACH_STEP || process.env.SLEEP_MS_AFTER_EACH_STEP_CI || 0
 };
