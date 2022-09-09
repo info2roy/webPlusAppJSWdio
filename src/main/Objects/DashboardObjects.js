9665835741fae9c3e519f59f2d7f8b712f3f5a76
Mahesh Bale <mahesh.bale@scripbox.com>
2022-09-09 18:00:10 +0530

    completed single end to end scenarios for RetireConfident form

--- src/main/Objects/DashboardObjects.js
+++ src/main/Objects/DashboardObjects.js
@@ -12,7 +12,7 @@ class DashboardObjects {
 
   returnToHome = { web: '//*[contains(text(),"Return to Home")]' };
 
-  homeButton = { web: '//button[text()="Home"]', app: '' };
+  homeButton = { web: '//button[text()="Home"]', app: '//android.widget.TextView[@text="HOME"]' };
 
   goToDashboardButton = { web: '//button[text()="Go to Dashboard"] | //button[text()="GO TO DASHBOARD"]' };
 
