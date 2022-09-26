987b6dea11b17612d9e0f93693fe95c9ea7864b4
Mahesh Bale <mahesh.bale@scripbox.com>
2022-09-26 11:05:05 +0530

    reverted changes in LoginData.js

--- src/config/data/structured/LoginData.js
+++ src/config/data/structured/LoginData.js
@@ -1,10 +1,8 @@
 module.exports = {
   username: 'test+180756@scripbox.io',
-  //password: 'folio334!cues',
-  password: 'Aaasssddd123$',
-  user180756: 'mahesh.bale@scripbox.com',
-  //user189182: 'test+189182@scripbox.io',
-  user189182: 'mahesh.bale@scripbox.com',
+  password: 'folio334!cues',
+  user180756: 'test+180756@scripbox.io',
+  user189182: 'test+189182@scripbox.io',
   user120405: 'test+120405@scripbox.io',
   user120406: 'test+120406@scripbox.io',
   user120409: 'test+120409@scripbox.io',
