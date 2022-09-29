e634a5c7bef0935f64c70734ffbeb9c79c3efd90
Priyankar <priyankar.roy@scripbox.com>
2022-09-29 14:32:20 +0530

    Commenting out initiateSIP feature

--- wdio.conf.js
+++ wdio.conf.js
@@ -27,7 +27,7 @@ exports.config = {
   // port: 52033,
   // path: '/wd/hub',
   specs: [
-    './src/test/features/MutualFunds/InitiateNewSIP.feature'
+    './src/test/features/LoginViaHomepage.feature'
   ],
   // Patterns to exclude.
   exclude: [
