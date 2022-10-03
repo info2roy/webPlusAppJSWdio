ab4f98c62604fb2c61b21a5fe7b0ee4c6b963c05
Mahesh Bale <mahesh.bale@scripbox.com>
2022-10-03 16:48:57 +0530

    moved step to CommonSteps.js

--- src/test/stepDefinitions/MoreOptions/PersonalInfoSteps.js
+++ src/test/stepDefinitions/MoreOptions/PersonalInfoSteps.js
@@ -1,7 +1,5 @@
 const { When, Then } = require('@wdio/cucumber-framework');
-const { expect } = require('chai');
 const PersonalInfoFunctionality = require('../../../main/Functionalities/MoreOptions/PersonalInfoFunctionality');
-const CommonFunctionality = require('../../../main/Functionalities/Common/CommonFunctionality');
 
 When(/^I update the profile picture$/, async () => {
   await console.log('When I update the profile picture');
@@ -20,11 +18,6 @@ Then(/^I click edit "([^"]*)?"$/, async (option) => {
 When(/^I enter data for (.+) with value (.+)$/, async (option, value) => {
   await console.log(`Entering data for ${option}`);
   await PersonalInfoFunctionality.updateProfileData(option, value);
-});
-
-Then(/^I see message (.+) for (.+)$/, async (uiError, data) => {
-  await console.log(`I see message ${uiError}`);
-  expect(await CommonFunctionality.validateUIMessage(uiError, data)).to.be.true;
 });
 
 Then(/^I update changes$/, async () => {
